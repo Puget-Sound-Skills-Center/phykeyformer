f33599a3cdb8032c9211af24a636c94ddd5a3b63
dakn3w <4031116@g.highlineschools.org>
2026-07-13 13:30:09 -0700

    ___
    <= 10  to  <= 6 (for playtesting reasons)

--- Assets/testscript.cs
+++ Assets/testscript.cs
@@ -5,7 +5,7 @@ public class testscript : MonoBehaviour
     [SerializeField] private LayerMask groundlayer;
     public bool isbuttonheld()
     {
-        if (Physics2D.BoxCast(transform.position, transform.localScale, transform.rotation.z, transform.up, 0, groundlayer)) { return true; }
+        if (Physics2D.BoxCast(transform.position, 1.01f*transform.localScale, transform.rotation.z, transform.up, 0, groundlayer)) { return true; }
         else { return false; }
     }
 }
